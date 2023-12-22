157df8073e14af6af5d47784d788c4f2bef2e17b
Akshit1311 <a4748g@gmail.com>
2023-12-23 01:49:33 +0530

    add: env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
-  lock: "0xbcc2533501bef44c2c050df0419c85ce30f928cb",
+  lock: process.env.NEXT_PUBLIC_LOCK_ADDR,
   network: 137,
 };
