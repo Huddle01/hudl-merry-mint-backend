1c6dc05e5446c6efa8c7eafc8ccde7f30fc550e7
Akshit1311 <a4748g@gmail.com>
2023-12-23 01:53:30 +0530

    fix: build errors

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
-  lock: process.env.NEXT_PUBLIC_LOCK_ADDR,
+  lock: process.env.NEXT_PUBLIC_LOCK_ADDR || "",
   network: 137,
-};
+} as const;
